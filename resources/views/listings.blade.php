af731cd958515110c33b3e7b84149637976b51c6
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-01-13 23:02:19 +0000

    learning - extend from a base template

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,11 +1,17 @@
+@extends('layout')
+@section('content')
+
 <h1>listings</h1>
 {{ $heading }}<br>
 @foreach ($listings as $listing)
 <a href="/listings/{{$listing['id']}}">
     {{ $listing['title'] }}<br>
-    {{ $listing['company'] }}
+    {{ $listing['company'] }}<br>
 </a>
+<br>
 @endforeach
 {{-- blade allows us to use twig syntax  --}}
 
 <!-- usual logic can go here, i.e. foreach to pull from arrays  -->
+
+@endsection

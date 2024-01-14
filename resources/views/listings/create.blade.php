b38fdaf0990c961d3cc8a5956aba2d8d368908a5
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-01-14 01:18:42 +0000

    learning - keep data in form on refresh

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -10,7 +10,9 @@
         <div class="mb-6">
           <label for="company" class="inline-block text-lg mb-2">Company Name</label>
           <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company"
-            value="{{old('company')}}" />
+            value="{{old('company')}}"
+            {{-- value field to keep data in form on refresh  --}}
+            />
 
           @error('company')
           <p class="text-red-500 text-xs mt-1">{{$message}}</p>
